c3ca21bc3cd5123b45f69ea09d1606e9b99358ae
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-10 17:47:17 +0200

    Done lesson on animationFrame

--- 02_code-canv-animation/index.js
+++ 02_code-canv-animation/index.js
@@ -17,6 +17,53 @@ const fireballImg = document.createElement("img");
 // "src" has to point as the image will be used in HTML file
 fireballImg.src = "fireball.png";
 
-fireballImg.addEventListener("load", () =>
-  ctx.drawImage(fireballImg, 50, 50, 150, 100)
-);
+let frameId;
+
+function draw(x = -75, y = canvas.height) {
+  if (frameId) cancelAnimationFrame(frameId);
+  // We must clear canvas to make sure no old graphics disturbs the new drawing
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(fireballImg, x, y, 150, 100);
+
+  x += 5;
+  y -= 5;
+
+  frameId = requestAnimationFrame(() => draw(x, y));
+  console.log(frameId);
+  if (frameId > 100) {
+    alert("You forgot to STOP!");
+    cancelAnimationFrame(frameId);
+  }
+}
+
+fireballImg.addEventListener("load", () => {
+  shootButton.addEventListener("click", () => draw());
+  stopButton.addEventListener("click", () => cancelAnimationFrame(frameId));
+});
+
+/* 
+let timeoutId;
+
+function draw(x = 0, y = 0) {
+  // We must clear canvas to make sure no old graphics disturbs the new drawing
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(fireballImg, x, y, 150, 100);
+
+  x += 5;
+  y += 2.5;
+
+  timeoutId = setTimeout(() => draw(x, y), 17); // 17 is 1000 ms divided by 60 frames/sec which is the normal refres rate of monitors
+}
+
+//fireballImg.addEventListener("load", () => {
+shootButton.addEventListener("click", () => {
+  if (timeoutId) clearTimeout(timeoutId);
+  draw();
+});
+
+stopButton.addEventListener("click", function () {
+  clearTimeout(timeoutId);
+});
+
+//});
+ */
